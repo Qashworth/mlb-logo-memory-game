44a941a9cb25c4eb25282fc15f15bd5702f90d2d
Qashworth <rashworth51@gmail.com>
2023-06-26 20:55:28 -0600

    baseline styling

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,36 @@
 import Image from 'next/image'
 import react from 'react'
-import { useState } from 'react'
+
+function Square({ value }) {
+  return <button className="square self-center p-2 m-2 h-32 w-64 border-2 rounded-xl justify-items-stretch">{value}</button>;
+}
 
 export default function Home() {
   return (
-    <main >
-      
-    </main>
+    <fragments>
+      <grid className="grid h-full w-full">
+        <div className="p-1 flex justify-around">
+          <Square/>
+          <Square/>
+        </div>
+        <div className="p-1 flex justify-around">
+          <Square/>
+          <Square/>
+        </div>
+        <div className="p-1 flex justify-around">
+          <Square/>
+          <Square/>
+        </div>
+        <div className="p-1 flex justify-around">
+          <Square/>
+          <Square/>
+        </div>
+        <div className="p-1 flex justify-around">
+          <Square/>
+          <Square/>
+        </div>
+      </grid>
+    </fragments>
   )
 }
+
