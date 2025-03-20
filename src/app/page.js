661db40904bda51eb302267f32317cd198ec763f
Qashworth <rashworth51@gmail.com>
2025-03-19 22:50:00 -0600

    refactor: move Square component definition to the end of page.js

--- src/app/page.js
+++ src/app/page.js
@@ -2,16 +2,6 @@
 import Image from 'next/image'
 import react from 'react'
 
-function Square({ value }) {
-  function handleClick() {
-    alert('You clicked me!');
-  }
-
-  return (
-  <button onClick={handleClick} className="square self-center p-2 m-2 h-32 w-64 border-2 rounded-xl justify-items-stretch">{value}</button>
-  );
-}
-
 export default function Home() {
   return (
     <fragments>
@@ -41,3 +31,12 @@ export default function Home() {
   )
 }
 
+function Square({ value }) {
+  function handleClick() {
+    alert('You clicked me!');
+  }
+
+  return (
+  <button onClick={handleClick} className="square self-center p-2 m-2 h-32 w-64 border-2 rounded-xl justify-items-stretch">{value}</button>
+  );
+}
